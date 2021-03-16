955666f6c6e305e48b1da98e13856a35639bf0c4
Jadon Calvert <calvertjadon@gmail.com>
2021-03-16 08:42:29 -0400

    Moved secret key to .env

--- geek_qa/settings.py
+++ geek_qa/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o4b845jheflw(tbbiv*-fd897kd=g34w_+20r^so!kawxap^!&'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
