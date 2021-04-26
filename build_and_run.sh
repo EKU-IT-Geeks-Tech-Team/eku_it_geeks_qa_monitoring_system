1a325c027a4698d77d28c41b614677b22aed86af
Jadon Calvert <calvertjadon@gmail.com>
2021-04-26 08:15:02 -0400

    remove old container before build

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,4 @@
 docker stop geeksqa
+docker rm geeksqa
 docker build -t geeks/qa .
 docker run -d --restart unless-stopped -p 80:8000 -v /root/eku_it_geeks_qa_monitoring_system/db.sqlite3:/usr/src/app/db.sqlite3 --name geeksqa geeks/qa
