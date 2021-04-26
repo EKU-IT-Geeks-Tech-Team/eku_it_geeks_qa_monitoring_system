332752163c0241da88cdd3f7c894b6267895eb25
Jadon Calvert <calvertjadon@gmail.com>
2021-04-26 08:13:39 -0400

    removed -rm flag

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,2 +1,3 @@
+docker stop geeksqa
 docker build -t geeks/qa .
-docker run --rm -d --restart unless-stopped -p 80:8000 -v /root/eku_it_geeks_qa_monitoring_system/db.sqlite3:/usr/src/app/db.sqlite3 --name geeksqa geeks/qa
+docker run -d --restart unless-stopped -p 80:8000 -v /root/eku_it_geeks_qa_monitoring_system/db.sqlite3:/usr/src/app/db.sqlite3 --name geeksqa geeks/qa
